cbb3af13ed6fe293a05e6a6e8f5085e4e185e8e6
Haikal <pancajiwa722@gmail.com>
2024-07-20 14:11:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eligendi quos nesciunt perspiciatis, sapiente ab veritatis harum exercitationem in perferendis amet, voluptatum odit doloribus dicta doloremque sed, ducimus consequuntur. Libero!</p>
+    <li>satu</li>
+    <li>dua</li>
 </body>
 </html>

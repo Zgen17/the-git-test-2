1f4cc3d2cf696e90cfee66c810e99cf2f21f758c
Haikal <pancajiwa722@gmail.com>
2024-07-20 14:06:55 +0700

    menambahkan satu paragraph

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eligendi quos nesciunt perspiciatis, sapiente ab veritatis harum exercitationem in perferendis amet, voluptatum odit doloribus dicta doloremque sed, ducimus consequuntur. Libero!</p>
+</body>
+</html>
